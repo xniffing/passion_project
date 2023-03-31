bf8be55233eeb2d68296e032796168a1016b630f
David Vicente <djvicente@gmail.com>
2023-03-31 16:03:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import * as _LIT from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+class DropDownCountry extends _LIT.LitElement {
+    static get properties() {
+        return {
+            msg: String
+        };
+    }
+    render() {
+        return _LIT.html`
+      <div>${this.msg}</div>
+    ;`
+    }
+}
+customElements.define('drop-down-country', DropDownCountry);
+
